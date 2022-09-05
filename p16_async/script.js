0abfa67fbdb8bced27fcbfcdc707858006651605
abrigante-dev <abrigante1234@pointloma.ede>
2022-09-05 10:33:54 -0700

    added promise async example

---
+++ p16_async/script.js
@@ -0,0 +1,86 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+// async timeout
+const p = document.getElementById('p');
+p.innerHTML = 'test test';
+// 5 second timeout
+setTimeout(() => {
+  p.innerHTML = 'It updated!';
+}, 5000);
+p.innerHTML = 'before update';
+
+const img = document.querySelector('.img');
+// img.src = 'img/img-1.jpg';
+
+// url
+// https://restcountries.com/v2/
+const apiURL = 'https://restcountries.com/v3.1/name/usa';
+const getCountry = country => {
+  // XML HTTP Request
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+  request.addEventListener('load', () => {
+    const [data] = JSON.parse(request.responseText);
+    // render country 1
+    renderCountry(data);
+
+    // // get neighbor country
+    const [neighbor] = data.borders;
+
+    if (!neighbor) return;
+
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbor}`);
+    request2.send();
+    request2.addEventListener('load', () => {
+      const [data2] = JSON.parse(request2.responseText);
+      renderCountry(data2);
+    });
+  });
+};
+
+// XML call
+const renderCountry = data => {
+  const html = `
+        <article class="country">
+          <img class="country__img" src="${data.flags.png}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>${data.population} people</span></p>
+            <p class="country__row"><span>🗣️</span>LANG</p>
+            <p class="country__row"><span>💰</span>CUR</p>
+          </div>
+        </article>
+  `;
+  // html insert adjacent
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+// getCountry('germany');
+// getCountry('usa');
+
+// modern AJAX call
+// promise: used a placeholder for a future result of an async function
+// promise does not need events or callback functions
+// promises can be chained instead of nested call backs
+// consume a promise
+const getCountryData = country => {
+  // fetch request for a promise
+  const request = fetch(`https://restcountries.com/v3.1/name/${country}`);
+  // .then function for a promise
+  request
+    // json function to be able to read the response body
+    .then(response => response.json())
+    // chain .then methods because .json function returns another promise
+    .then(data => renderCountry(data[0]));
+};
+
+getCountryData('usa');
+getCountryData('canada');
